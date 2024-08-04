fcf66a1f14f2d46b5e44076dd00ae09f3f58683d
Bhavin Nandaniya <bhavinnandaniya@hotmail.com>
2024-08-04 22:56:49 +0530

    added create, get, get by id, get by category feature in catalog API

---
+++ srs/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProducrsResult>;
+    public record CreateProducrsResult(Guid Id);
+
+    internal class CreateProductCommandHandler(IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProducrsResult>
+    {
+        public async Task<CreateProducrsResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // create Product entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            // return the result
+            return new CreateProducrsResult(product.Id);
+        }
+    }
+}
